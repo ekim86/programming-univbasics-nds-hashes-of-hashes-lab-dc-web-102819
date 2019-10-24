efda305eeee27891e49d90bb283d961497ddfd68
eunice kim <oonis86@gmail.com>
2019-10-24 16:41:54 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,28 +33,28 @@
 def naming_system
 PORTION_1 = {
    :label => "Kingdom",
-   sub_category: {
-     label: "Phylum",
-     sub_category: {
-       label: "Class"
+   :sub_category => {
+     :label => "Phylum",
+     :sub_category => {
+       :label => "Class"
      }
    }
  }
  
 PORTION_2 = {
-   label: "Order"
+   :label => "Order"
  }
  
  PORTION_3 = {
-   label: "Family",
-   sub_category: {
-     label: "Genus",
+   :label => "Family",
+   :sub_category => {
+     :label => "Genus",
    }
  }
  
 PORTION_4 = {
-   label: "Species",
-   sub_category: nil
+   :label => "Species",
+   :sub_category => nil
  }
 end
   
